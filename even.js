37cf3b707b438da8f7b88d12e1aa1c8bb541f940
Stanimir Angelov <62446471+Angelov1912@users.noreply.github.com>
2022-02-13 14:34:04 +0000

    Update even.js

--- even.js
+++ even.js
@@ -1,4 +1,6 @@
 //Print all even numbers from 1 to 100
+//To print all odd numbers change the condition with the following:
+// if (i % 2 !== 0)
 
 for (let i = 1; i <=100; i++) {
 	if (i % 2 == 0) {
